f1f65a017b7b6af1f50b1b735121299b3dfa4446
Openxcell-1234 <161296995+Openxcell-1234@users.noreply.github.com>
2024-04-08 13:44:25 +0530

    new code commit log4j

--- Cribzzzz/src/test/java/testClass/BeforeAfterTest.java
+++ Cribzzzz/src/test/java/testClass/BeforeAfterTest.java
@@ -1,5 +1,10 @@
 package testClass;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.testng.Assert;
 import org.testng.ITestResult;
 import org.testng.annotations.AfterMethod;
@@ -10,35 +15,45 @@ import org.testng.annotations.BeforeSuite;
 //import report.ExtentManager;
 import testBase.TestBase;
 
-public class BeforeAfterTest extends TestBase{
-	
+public class BeforeAfterTest extends TestBase {
+
+	static {
+
+		SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy-hh-mm-ss");
+		System.setProperty("current.date.time", dateFormat.format(new Date()));
+	}
+
+	public static Logger log = Logger.getLogger(BeforeAfterTest.class);
+
 	@BeforeSuite
 	public void beforeSuite() {
+		PropertyConfigurator.configure(System.getProperty("user.dir") + "/src/main/java/log.properties");
 //		ExtentManager.onStart();
 	}
-	
+
 	@BeforeMethod
 	public void beforeMethod(ITestResult result) {
 //		ExtentManager.onTestSuccess(result.getMethod().getMethodName());
 		prerequisite();
+		log.info("Driver triggered");
 	}
-	
+
 	@AfterMethod
 	public void tearDown(ITestResult result) {
-		if(result.getStatus() == ITestResult.FAILURE) {
+		if (result.getStatus() == ITestResult.FAILURE) {
 //			ExtentManager.onTestFailure(result.getMethod().getMethodName());
 			Assert.fail(result.getThrowable().getMessage());
-		}else if(result.getStatus() == ITestResult.SKIP) {
+		} else if (result.getStatus() == ITestResult.SKIP) {
 //			ExtentManager.onTestSkipped(result.getMethod().getMethodName());
-		}else if(result.getStatus() == ITestResult.SUCCESS) {
-			
+		} else if (result.getStatus() == ITestResult.SUCCESS) {
+
 		}
-		
+
 		driver.close();
 	}
-	
+
 	@AfterSuite
-	public void  afterSuite() {
+	public void afterSuite() {
 //		ExtentManager.onFlush();
 	}
 
--- Cribzzzz/src/test/java/testClass/HomePageTest.java
+++ Cribzzzz/src/test/java/testClass/HomePageTest.java
@@ -1,11 +1,16 @@
 package testClass;
 
+import org.apache.log4j.Logger;
 import org.testng.annotations.Test;
 
 public class HomePageTest extends BeforeAfterTest{
 	
+	public static Logger log = Logger.getLogger(HomePageTest.class);
+	
 	@Test
 	public void loginTestMethod() {
+		
+		log.info("test executed");
 		
 	}
 
